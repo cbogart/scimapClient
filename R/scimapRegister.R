5c9cfcf4b04362295dab2d3e4934ea77e938934a
Chris Bogart <cbogartdenver@gmail.com>
2014-09-18 17:41:05 -0400

    First work towards v0.1.4; accumulate activity until the end of the script, then send when the user exits R

--- R/scimapRegister.R
+++ R/scimapRegister.R
@@ -1,9 +1,42 @@
 
-reghost = "sci-net-map.isri.cmu.edu"
+#reghost = "sci-net-map.isri.cmu.edu"
+reghost = "0.0.0.0"
 regport = 7778
 
-idfile <- function() { return (paste(system.file(package="scimapClient"),"install_unique_id",sep="/")) }
-okfile <- function() { return (paste(system.file(package="scimapClient"),"permission_granted",sep="/")) }
+jobinf <- new.env()
+jobinf$jobID = ""
+jobinf$stack = list()
+jobinf$staticDeps = list()
+jobinf$dynamicDeps = list()
+jobinf$weakDeps = list()
+jobinf$dynamicPackageDeps = list()
+jobinf$weakPackageDeps = list()
+jobinf$userRunMetadata = list()
+jobinf$last = ""
+
+getJobID <- function() {
+   if (jobinf$jobID == "") {
+       jobinf$jobID = paste(getScimapId(), 
+                     as.character(as.integer(Sys.time())), 
+                     sep="")
+   }
+   return(jobinf$jobID)
+}
+
+sendRegistrationInfo <- function() {
+   topCall <- toString(sys.call(which=1)); 
+   scimapRegister(topCall)
+}
+
+e <- new.env()
+.onLoad <- function(lib, pkg, ...) {
+    reg.finalizer(e, function (obj) {
+        sendRegistrationInfo()
+    }, onexit=TRUE)
+}
+
+idfile <- function() { return (paste(system.file(package="scimapClient"),"../scimap_unique_id",sep="/")) }
+disabledFile <- function() { return (paste(system.file(package="scimapClient"),"../scimap_permission_denied",sep="/")) }
 
 getScimapId <- function() {
     if (file.exists(idfile())) {
@@ -17,6 +50,15 @@ getScimapId <- function() {
 
 enableScimap <-
 function() {
+    if (!interactive()) {
+       cat("The function enableScimap() only works in interactive mode.")
+       return()
+    }
+    if (isEnabledScimap()) {
+       cat("Already enabled.\nCall disableScimap() if you want to disable it.\n")
+       return()
+    }
+    
 cat("This package sends anonymous usage tracking information about R packages
 to a server that allows authors of packages to track how
 widely used and installed they are.  This is helpful
@@ -30,72 +72,124 @@ scimapRegister() for more information (type: help(scimapRegister) )
 ")
     
     ok <- readline(paste("Is it OK to send anonymous usage reports to ", reghost, "? (y/n) ", sep=""))
-    if (!interactive()) {
-       cat("The function enableScimap() only works in interactive mode.")
-       return()
-    }
-    if (isEnabledScimap()) {
-       cat("Already enabled.\nCall disableScimap() if you want to disable it.\n")
-       return()
-    }
     if (substr(ok, 1, 1) == "y" || substr(ok, 1, 1) == "Y") {
-       write("OK",file=okfile())
-       cat("***Done! Usage reporting enabled.***
-       
-     Please insert a call to scimapRegister() in your scripts such that they 
-     will be called ONCE per run, after R loads all the packages the script 
-     will use.\n")
-    } else {
-       if (isEnabledScimap()) {
-           file.remove(okfile())
+       if (!isEnabledScimap()) {
+           file.remove(disabledFile())
        }
-       cat("***Not enabled.  No usage reports will be sent.***\n")
+       cat("***Done! Usage reporting enabled.***")
+    } else {
+       write("Disabled",file=disabledFile())
+       cat("***Disabled.  No usage reports will be sent.***\n")
     }
 }
 
 isEnabledScimap <-
 function() {
-    return(file.exists(okfile()))
+    return(!file.exists(disabledFile()))
 }
 
 disableScimap <-
 function() {
-    file.remove(okfile())
+    write("Disabled",file=disabledFile())
     cat("Usage reporting has been disabled.\nCall enableScimap() to renable it at any time.\n")
 }
 
-scimapRegister <-
-function(programName=topCall ) {
-    if (isEnabledScimap()) {
-        topCall <- toString(sys.call(which=1)); 
-        uniqueId = getScimapId()
-        username <- uniqueId  #Sys.info()["user"]
-        commandline <- commandArgs()
-        t <- Sys.time()
-        unixtime <- as.character(as.integer(t))
-        fmtTime <- format(t, "%a %b %e %H:%M:%S %Y")
-        deps = c(lapply( (.packages()), function (p) {
-              return (paste(p, packageVersion(p), sep="/"))
-          } ), paste("R/", R.version[["major"]], ".", R.version[["minor"]], sep=""))   
-        jobrec <- list(
-          user = username,
+
+note <- function(array, key, value) {
+    if (is.null(key) || is.null(value)) {
+        return(array);
+    }
+    if (key == "" || value == "") {
+        return(array);
+    }
+    if (key == value) {
+        return(array);
+    }
+    if (!key %in% names(array)) {
+        array[[key]] = c()
+    }
+    array[[key]] = unique(c(array[[key]], value))
+    return(array)
+}
+
+guessPackage <- function(f) {
+   if (typeof(f) == "character") {
+      return("");
+   }
+   e1 <- environment(f)
+   if (environmentName(e1) == "") {
+      return (environmentName(parent.env(e1)));
+   } else {
+      return (environmentName(e1));
+   }
+}
+
+scimapIn <- function(caller=parent) {
+    parent = toString(sys.call(which=-1))
+   
+    if (!is.null(jobinf$last)) {
+        jobinf$weakDeps = note(jobinf$weakDeps, toString(caller), toString(jobinf$last));
+        jobinf$weakPackageDeps = note(jobinf$weakPackageDeps, guessPackage(caller), guessPackage(jobinf$last))
+    }
+    if (length(jobinf$stack) > 0) {
+        jobinf$dynamicDeps = note(jobinf$dynamicDeps, toString(jobinf$stack[[length(jobinf$stack)]]), toString(caller));
+        jobinf$dynamicPackageDeps = note(jobinf$dynamicPackageDeps, guessPackage(jobinf$stack[[length(jobinf$stack)]]), guessPackage(caller));
+    }
+    jobinf$stack = c(jobinf$stack, caller)
+    jobinf$last = NULL
+}
+
+scimapOut <- function(caller=parent) {
+    parent = toString(sys.call(which=-1))
+
+    length(jobinf$stack) <- length(jobinf$stack)-1
+    jobinf$last = caller
+}
+
+getPackageDependencies <- function() {
+        ip <- installed.packages()
+        deps = list()
+        for (p in (.packages())) { 
+            deps[[paste(p, packageVersion(p), sep="/")]] = c(tools::package_dependencies(p, db=ip)[[p]])
+        }
+        deps[[paste("R/", R.version[["major"]], ".", R.version[["minor"]], sep="")]] = c("utils")   
+        return(deps)
+}
+
+scimapPacket <-
+function(programName) {
+    t <- Sys.time()
+    unixtime <- as.character(as.integer(t))
+    fmtTime <- format(t, "%a %b %e %H:%M:%S %Y")
+    jobrec <- list(
+          scimapInfoVersion = 3,
+          user = getScimapId(),
           startEpoch = unixtime,
           startTime = fmtTime,
           platform = list(
               system = Sys.info()[["sysname"]],
               version = Sys.info()[["release"]],
               hardware = Sys.info()[["machine"]]),
-          account = uniqueId,
           exec = programName, 
-          jobID = paste(username,unixtime, sep=""),
-          pkgT = deps
-        )
+          jobID = getJobID(),
+          pkgT = getPackageDependencies(),
+          dynDeps = RJSONIO::toJSON(jobinf$dynamicDeps),
+          dynPackDeps = RJSONIO::toJSON(jobinf$dynamicPackageDeps),
+          weakDeps = RJSONIO::toJSON(jobinf$weakDeps),
+          weakPackDeps = RJSONIO::toJSON(jobinf$weakPackageDeps)
+    )
+    return(RJSONIO::toJSON(jobrec));
+}
+
+scimapRegister <- function(programName = topCall) {
+    topCall <- toString(sys.call(which=1)); 
+    if (isEnabledScimap()) {
         result = tryCatch({
           a <- make.socket(reghost, regport)
           on.exit(close.socket(a))
-          write.socket(a, RJSONIO::toJSON(jobrec))
+          write.socket(a, scimapPacket(programName))
         }, error = function(e) {
-          cat("scimapRegister connection failure: not a problem.\n")
+          cat("Could not upload usage data to ", reghost, ".\n")
         })
     }
 }
